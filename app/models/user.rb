1f73dae066f428f01c98f7618657e54912210d76
Jay Deeh <jaad.mateo15@gmail.com>
2017-07-26 08:28:18 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+has_many :twits
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "profile.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, 
+           :validatable, :authentication_keys => [:login]         
+  
+  attr_accessor :login
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end  
+  
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  } 
+  
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+    
+end
---
+++ db/migrate/20170726050926_devise_create_users.rb
@@ -0,0 +1,3 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+ 
+end
